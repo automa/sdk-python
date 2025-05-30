d341375259a7693dfabf9ccdf6e586a4c35f6723
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2025-05-30 20:27:34 +0530

    Change logic for webhook signatures based on standard webhooks

--- packages/bot/src/automa/bot/webhook.py
+++ packages/bot/src/automa/bot/webhook.py
@@ -1,5 +1,8 @@
+import base64
 import hashlib
 import hmac
+import json
+from datetime import datetime
 
 
 def verify_webhook(secret: str, signature: str, payload: str) -> bool:
@@ -11,13 +14,41 @@ def verify_webhook(secret: str, signature: str, payload: str) -> bool:
     ):
         return False
 
+    if not secret.startswith("atma_whsec_"):
+        raise ValueError("Secret must start with 'atma_whsec_'")
+
+    signatures = signature.split(" ")
     generated_signature = generate_webhook_signature(secret, payload)
 
     # Use constant-time comparison to prevent timing attacks
-    return hmac.compare_digest(generated_signature, signature)
+    return any(hmac.compare_digest(generated_signature, sig) for sig in signatures)
 
 
 def generate_webhook_signature(secret: str, payload: str) -> str:
-    return hmac.new(
-        secret.encode("utf-8"), payload.encode("utf-8"), hashlib.sha256
-    ).hexdigest()
+    try:
+        msg = json.loads(payload)
+    except json.JSONDecodeError:
+        raise ValueError("Invalid payload format")
+
+    if not isinstance(msg, dict):
+        raise ValueError("Invalid payload format")
+
+    id = msg.get("id")
+    timestamp = msg.get("timestamp")
+
+    if not id or not timestamp:
+        raise ValueError("Payload must contain both 'id' and 'timestamp' fields")
+
+    # Convert timestamp from ISO to epoch time in seconds
+    dt = datetime.fromisoformat(timestamp.replace("Z", "+00:00"))
+    timestamp = int(dt.timestamp())
+
+    sig = base64.b64encode(
+        hmac.new(
+            secret[11:].encode("utf-8"),
+            f"{id}.{timestamp}.{payload}".encode("utf-8"),
+            hashlib.sha256,
+        ).digest()
+    ).decode("utf-8")
+
+    return f"v1,{sig}"
